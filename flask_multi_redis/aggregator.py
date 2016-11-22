890a868785368cca2375408285d2b97015fbf516
Thomas Sarboni <max-k@post.com>
2016-11-22 14:35:37 +0100

    Correct aggregator module

--- flask_multi_redis/aggregator.py
+++ flask_multi_redis/aggregator.py
@@ -42,14 +42,7 @@ class Aggregator(object):
             item = self._output_queue.get()
             self._output_queue.task_done()
             results.append(item)
-        if results != [] or target.__name__ == '_keys':
-            if target.__name__ == '_scan_iter':
-                return chain(*results)
-            if target.__name__ == '_delete':
-                return sum([x for x in results if isinstance(x, int)])
-            if target.__name__ == '_set':
-                return len(set(results)) <= 1
-            return results
+        return results
 
     def get(self, pattern):
         """Aggregated get method."""
@@ -67,29 +60,34 @@ class Aggregator(object):
         def _keys(node, pattern):
             for result in node.keys(pattern):
                 self._output_queue.put(result)
-        # return list(OrderedDict.fromkeys(self._runner(_keys, pattern)))
-        return sorted(list(unique_everseen(self._runner(_keys, pattern))))
+        results = self._runner(_keys, pattern)
+        # return list(OrderedDict.fromkeys(results))
+        return sorted(list(unique_everseen(results)))
 
     def set(self, key, pattern, **kwargs):
         """Aggregated set method."""
         def _set(node, pattern, key=key, **kwargs):
             self._output_queue.put(node.set(key, pattern, **kwargs))
-        return self._runner(_set, pattern, **kwargs)
+        results = self._runner(_set, pattern, **kwargs)
+        return len(set(results)) <= 1
 
     def _aggregated_put(self, pattern, method):
         """Default aggregated put method."""
-        def _aggregated_method(node, pattern, method):
+        def _aggregated_method(node, pattern):
             _method = getattr(node, method)
-            self._output_queue(_method(pattern))
-        return self._runner(_aggregated_method, pattern, method)
+            self._output_queue.put(_method(pattern))
+        return self._runner(_aggregated_method, pattern)
 
     def delete(self, pattern):
         """Aggregated delete method."""
-        self._aggregated_put(pattern, 'delete')
+        results = self._aggregated_put(pattern, 'delete')
+        return sum([x for x in results if isinstance(x, int)])
 
     def scan_iter(self, pattern):
         """Aggregated scan_iter method."""
-        self._aggregated_put(pattern, 'scan_iter')
+        results = self._aggregated_put(pattern, 'scan_iter')
+        print(results)
+        return chain(*results)
 
     def __getattr__(self, name):
         if name in ['_redis_client', 'connection_pool']:
